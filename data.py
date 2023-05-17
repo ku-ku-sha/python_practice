6082c309b48187d48d2999ee8fd7eda7f19df2a3
pa.cybaeva <kukushkina.p@yandex.ru>
2023-05-17 10:01:41 +0300

    first task from module BuildIn functions

--- data.py
+++
@@ -1,3 +0,0 @@
-class DataFunctions:
-    delete_a = ('', 'iouhlkjhjkg;ljlkji', 'AAAaaaаааААА', 'jkhlkhl;aaaljlA')
-    expected_res_delete_a = (('', 0), ('iouhlkjhjkg;ljlkji', 0), ('', 12), ('jkhlkhl;aaaljlA', 4))
