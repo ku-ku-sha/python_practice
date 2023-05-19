47b9f56b3fcad6e5edd892bf50fb4fbb1b044c26
pa.cybaeva <kukushkina.p@yandex.ru>
2023-05-19 14:24:12 +0300

    feat: добавлены новые функции

--- tasks.py
+++ tasks.py
@@ -1,7 +1,9 @@
 from typing import Union
+import os
 
-
+#
 # Встроенные функции python
+#
 def delete_a(n: str) -> Union[tuple, int]:
     '''В строке удалить все буквы "а"  и подсчитать количество удаленных символов
     Функция удаляет строчные и заглавные буквы а английского и русского алфавита, результатом является кортеж (<строка, которая не содержит а>, <количество удаленных символов>)'''
@@ -15,25 +17,22 @@ def delete_a(n: str) -> Union[tuple, int]:
         return -1
 
 
-def phone_number(n: str) -> Union[bool, int]:
+def phone_number(n: str):
     '''Проверить, что номера телефонов состоят только из цифр. Они могут начинаться с «+», цифры могут быть разделены пробелами и дефисами «-»
     Например: 8-999-777-1111, +7 999 333 2222, +7 999-555-11-11'''
     from re import fullmatch
-    num = '0123456789+- '
-    try:
-        if len(n) == 0 or n.strip(
-                num) != 0:  # проверка того, что ввод непустой и не содержит символов, кроме цифр, пробелов, символов "+" и "-" - блок удовлетворяет условию задания
-            print('Invalid phone number')
+    invalid_symbols = ''.join(filter(lambda x: x not in '0123456789+- ', n))
+    numbers = ''.join(filter(lambda x: x.isalpha(), n))
+    if invalid_symbols != '' or len(
+            numbers) != 11:  # данные не содержат символов, кроме цифр, пробелов, символов "+" и "-", количество цифр соответствует формату
+        print('Invalid phone number')
+        return False
+    else:  # проверка соответствия номера формату (используются регулярные выражения)
+        phone_format = r'[+]?([7|8])[ -]([0-9]{3})[- ]([0-9]{3})[- ]([0-9]{2})(-)?([0-9]{2})'  # подходит для всех вариантов номера, перечисленных в примере
+        if fullmatch(phone_format, n)[0] == n:
+            return True
+        else:
             return False
-        else:  # проверка соответствия номера формату (используются регулярные выражения)
-            phone_format = r'[+]?([7|8])[ -]([0-9]{3})[- ]([0-9]{3})[- ]([0-9]{2})(-)?([0-9]{2})' #подходит для всех вариантов номера, перечисленных в примере
-            if fullmatch(phone_format, n) == n:
-                return True
-            else:
-                return False
-    except TypeError:
-        print('Invalid input type')
-        return -1
 
 
 def get_file_name(n: str) -> Union[str, int]:
@@ -45,7 +44,8 @@ def get_file_name(n: str) -> Union[str, int]:
             return ''
         else:
             result = n.split('\\')
-            if '.' not in result[-1]:  # при отсутствии точки, последнее значение является именем папки
+            if '.' not in result[-1] or result[-1][
+                0] == '.':  # при отсутствии точки или если точка в начале, последнее значение является именем папки
                 print('File name not found')
                 return ''
             else:
@@ -56,76 +56,150 @@ def get_file_name(n: str) -> Union[str, int]:
         return -1
 
 
-def swap_min_max(n: list) -> Union[list, int]:
+def swap_min_max(n: list) -> Union[int, list]:
     '''Поменять местами самый большой и самый маленький элементы списка
     Функция позволяет обрабатывать списки строк и списки чисел'''
     if isinstance(n, list):
-        type_n = type(n[0])
-        match str(type_n):
+        type_n = str(type(n[0])).split("'")[1]
+        match type_n:
             case 'str':
-                check_all = list(map(lambda x: type(x) == str, n))#проверка: все элементы можно сравнить с начальным (вариант с циклом менее затратный тк прерывается при нахождении 1го эл-та, не соответствующего условию, представлен в ветке ---)
+                check_all = list(map(lambda x: type(x) == str,
+                                     n))  # проверка: все элементы можно сравнить с начальным (вариант с циклом менее затратный тк прерывается при нахождении 1го эл-та, не соответствующего условию, представлен в ветке ---)
                 if all(check_all):
                     min_el = min(n)
                     max_el = max(n)
-                    n[n.index(min_el)], n[n.index(max_el)] = n[n.index(max_el)], n[n.index(min_el)] #меняем местами мин и макс
-                    return n
+                    res = n[:]
+                    print(n.index(min_el), max_el)
+                    res[n.index(min_el)], res[n.index(max_el)] = res[n.index(max_el)], res[
+                        n.index(min_el)]  # меняем местами мин и макс
+                    return res
                 else:
                     print('List elements cannot be compared')
                     return -1
             case 'int' | 'float':
-                check_all = list(map(lambda x: type(x) in (int, float)), n)
+                check_all = list(map(lambda x: type(x) in (int, float), n))
                 if all(check_all):
                     min_el = min(n)
                     max_el = max(n)
-                    n[n.index(min_el)], n[n.index(max_el)] = n[n.index(max_el)], n[n.index(min_el)] #меняем местами мин и макс
-                    return n
+                    res = n[:]
+                    print(n.index(min_el), max_el)
+                    res[n.index(min_el)], res[n.index(max_el)] = res[n.index(max_el)], res[
+                        n.index(min_el)]  # меняем местами мин и макс
+                    return res
                 else:
                     print('List elements cannot be compared')
                     return -1
             case _:
                 print('List elements cannot be compared')
                 return -1
-
     else:
         print('Invalid input type (List required)')
         return -1
 
-def most_expensive_product(n: dict):
-    '''Дан список словарей:
-# [{“наименование”: “Спички”, “цена”: 1},  {“наименование”: “Лук”, “цена”: 37},  … , {“наименование”: str, “цена”: int}]
-# Найти ТОП-2 самых дорогих товаров и вывести в том же формате.'''
+
+def most_expensive_product(n: list):
+    ''' Дан список словарей: [{“наименование”: “Спички”, “цена”: 1},  {“наименование”: “Лук”, “цена”: 37},  … , {“наименование”: str, “цена”: int}]
+    Найти ТОП-2 самых дорогих товаров и вывести в том же формате.'''
     if len(n) <= 2:
         return n
-    while len(n) > 2:
-        pass
+    else:
+        result = []
+        most_expensive_prod = max(n, key=lambda x: x['цена'])  # поиск самого дорого товара
+        result.append(most_expensive_prod)  # добавление в список результатов
+        del n[n.index(most_expensive_prod)]  # удаление найденного из исходного списка
+        most_expensive_prod = max(n, key=lambda x: x['цена'])
+        result.append(most_expensive_prod)
+        return result
+
 
 def count_lucky_tickets() -> int:
     '''Напишите функцию, которая подсчитывает количество счастливых шестизначных билетов'''
-    sums = [i for i in range(0, 28)] # варианты сумм 3х чисел 0-9
+    sums = [0 for i in range(0,
+                             28)]  # максимальная сумма 3 чисел 0-9 = 27, минимальная = 0. Индекс списка - Сумма, значение - количество вариантов получения этой суммы из 3 чисел
+    for i in range(0, 1000):
+        sum_3 = i % 10 + i % 100 // 10 + i // 100
+        sums[sum_3] += 1
+    return sum(map(lambda x: x ** 2, sums))
 
 
 def func_time(func):
-    start_time =
-    func
-    end_time =
-    return end_time - start_time
+    '''Напишите декоратор func_time, который подсчитывает и выводит сколько времени выполняется функция, обернутая в него.'''
+    from datetime import datetime
+    def inner(*args, **kwargs):
+        start_time = datetime.now()
+        func(*args, **kwargs)
+        end_time = datetime.now()
+        print(f'Функция {func.__name__} выполнялась {end_time - start_time}')
+
+    return inner
 
 
-1) Получить список файлов в папке
 
-2) Получить текущую директорию
 
-3) Создать папку
+#
+# Модуль os
+#
 
-4) Склеить путь из папки и файла
 
-5) Посчитать сколько в каталоге установки python:•папок•файлов с расширением ".py"•файлов с расширением ".exe"•всего файлов•Записать результаты в файл
+def get_file_list(dir_path):
+    '''Получить список файлов в папке'''
+    from os import listdir
+    from os.path import isfile
+    from os.path import join as os_join
+    file_list = list(filter(lambda x: isfile(os_join(dir_path, x)), listdir(dir_path)))
+    print(f'Список файлов в папке: {file_list}')
+    return file_list
 
-1) Получить текущую дату +5 дней в формате ДД.ММ.ГГ
-2) Получить текущую дату в формате ДД.ММ.ГГГГ
-3) На входе есть строка '03.05.13' надо к этой дате прибавить 10 дней и вывести в формате ДД.ММ.ГГГГ
-4) Вывести дату в формате ДД.ММ.ГГ, эта дата должна быть первым днем месяца
-5) Вывести дату в формате ДД.ММ.ГГ, эта дата должна быть последним днем месяца
-6) Прибавить к любой дате 1 месяц и вывести в формате ДД.ММ.ГГГГ
-7) Написать функцию change_month, которая к переданной дате прибавляет/вычитает переданное кол-во месяцев.Например:
-change_month(’12.12.12’, 7) -> ’12.07.13’ change_month(’01.11.10’, -5) -> ’01.06.10’
+def print_current_directory():
+    '''Получить текущую директорию'''
+    print(f'Текущая директория {os.getcwd()}')
+
+def create_folder(path)
+    '''Создать папку'''
+    os.mkdir(path) # указать путь
+
+def get_full_path(path, file):
+    '''Склеить путь из папки и файла'''
+    print(os.path.join(path, file))
+
+
+def count_python(path=r'C:\Python'):
+    '''Посчитать сколько в каталоге установки python: 1) папок 2)файлов с расширением ".py" 3) файлов с расширением ".exe" 4) всего файлов
+    Записать результаты в файл'''
+    res = {'folders': 0, 'py_files': 0, 'exe_files': 0, 'files': 0}
+    for i in os.walk(path):
+        res['folders'] += 1
+        res['files'] += len(i[2])
+        res['py_files'] += len(list(filter(lambda x: x.endswith('.py'), i[2])))
+        res['exe_files'] += len(list(filter(lambda x: x.endswith('.exe'), i[2])))
+    print('folders'.ljust(10), res['folders'] - 1)
+    print('files'.ljust(10), res['files'])
+    print('py_files'.ljust(10), res['py_files'])
+    print('exe_files'.ljust(10), res['exe_files'])
+
+
+
+
+
+
+#
+#Модуль datetime
+#
+'''Получить текущую дату +5 дней в формате ДД.ММ.ГГ'''
+
+
+'''Получить текущую дату в формате ДД.ММ.ГГГГ'''
+
+'''На входе есть строка '03.05.13' надо к этой дате прибавить 10 дней и вывести в формате ДД.ММ.ГГГГ'''
+
+'''Вывести дату в формате ДД.ММ.ГГ, эта дата должна быть первым днем месяца'''
+
+'''Вывести дату в формате ДД.ММ.ГГ, эта дата должна быть последним днем месяца'''
+
+'''Прибавить к любой дате 1 месяц и вывести в формате ДД.ММ.ГГГГ'''
+
+'''Написать функцию change_month, которая к переданной дате прибавляет/вычитает переданное кол-во месяцев.
+Например:
+   change_month(’12.12.12’, 7) -> ’12.07.13’ 
+   change_month(’01.11.10’, -5) -> ’01.06.10’ 
+'''
